759593121417234e847fb6755e1056da26a027ad
jose carlos <josecar400@gmail.com>
2025-05-20 21:35:17 +0200

    #3 funcion gradosARadianes

--- logica.js
+++ logica.js
@@ -2,3 +2,8 @@ function calculaDistancia(angulo, velIni){
     let distancia=(Math.pow(velIni)*Math.sin(2*angulo))/9.81;
     return distancia;
 }
+
+function gradosARadianes(grados){
+    let radianes=(180/Math.PI)*grados;
+    return radianes;
+}
