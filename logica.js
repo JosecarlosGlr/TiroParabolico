e75360f7bd95a1a6a4c5bebcf6715738c6a4544c
jose carlos <josecar400@gmail.com>
2025-05-23 16:37:15 +0200

    #3 funciones gradosaradianes integrada en calculadistancia

--- logica.js
+++ logica.js
@@ -1,9 +1,9 @@
-function calculaDistancia(angulo, velIni){
-    let distancia=(Math.pow(velIni)*Math.sin(2*angulo))/9.81;
-    return distancia;
-}
-
 function gradosARadianes(grados){
     let radianes=(Math.PI/180)*grados;
     return radianes;
+}
+function calculaDistancia(angulo, velIni){
+    let angulorad=gradosARadianes(angulo);
+    let distancia=(Math.pow(velIni,2)*Math.sin(2*angulorad))/9.81;
+    return distancia;
 }
