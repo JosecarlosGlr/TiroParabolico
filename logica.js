5f91498c118666f933c4697b42216420baaaa7fc
jose carlos <josecar400@gmail.com>
2025-05-23 17:13:50 +0200

    #4 implementación de validación de datos

--- logica.js
+++ logica.js
@@ -7,3 +7,22 @@ function calculaDistancia(angulo, velIni){
     let distancia=(Math.pow(velIni,2)*Math.sin(2*angulorad))/9.81;
     return distancia;
 }
+function calcular() {
+     angulo = parseFloat(document.getElementById('angulo').value);
+    const velocidad = parseFloat(document.getElementById('velocidad').value);
+
+    if (isNaN(angulo) || isNaN(velocidad)) {
+        document.getElementById('resultado').textContent = "Por favor introduce valores numéricos válidos.";
+        return;
+    }
+    else if ((angulo)<90 || (angulo)>0 || (velocidad)>0) {
+        document.getElementById('resultado').textContent = "Por favor el angulo debe ser mayor de 0 y menor de 90º.";
+        return;
+    }
+    else if ((velocidad)>0) {
+        document.getElementById('resultado').textContent = "Por favor introduce una velocidad positiva";
+        return;
+    }
+    const distancia = calculaDistancia(angulo, velocidad);
+    document.getElementById('resultado').textContent = `La distancia es: ${distancia} metros.`;
+}
